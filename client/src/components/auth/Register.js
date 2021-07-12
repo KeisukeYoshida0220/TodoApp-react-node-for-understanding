610ecf7bf534118679a23b3eebeb254ec3f8fc82
Keisue <keisuke@wowinc.app>
2021-07-13 08:20:48 +0900

    add register and routing

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = (props) => {
+  return(
+    <div className="form-login">
+      <p>hogehojoejoge</p>
+    </div>
+  );
+}
+
+export default Register;
